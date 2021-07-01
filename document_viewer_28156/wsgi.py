bb54c728949667f57e8709923c9aa9907fc7c945
Crowdbotics <team@crowdbotics.com>
2021-07-01 04:56:04 +0000

    Update models

--- document_viewer_28156/wsgi.py
+++ document_viewer_28156/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'document_viewer_28156.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "document_viewer_28156.settings")
 
 application = get_wsgi_application()
